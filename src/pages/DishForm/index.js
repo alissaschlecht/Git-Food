00805167f4aa2290b77c8831119205ce067eea61
Alissa Schlecht <alissaschlecht@Cathleens-MacBook-Pro.local>
2019-10-04 19:03:37 -0400

    add new dish wip

--- src/pages/DishForm/index.js
+++ src/pages/DishForm/index.js
@@ -2,14 +2,40 @@ import React from 'react';
 import styles from './dishForm.module.scss';
 import { Link } from "react-router-dom";
 import Input from '../../components/Input/input';
+import Text from '../../components/Text/Text';
+import Wrapper from '../../components/Wrapper';
+import Button from '../../components/Button/Button';
 
 const DishForm = ({ children, className }) => {
     return (
-        <div className={`${styles.dish} ${className}`}>
-            I'm gonna be a form!!
-            <Input />
+        <Wrapper>
             <Link to="/" className={styles.button}>View all dishes</Link>
-        </div>
+            <Text.Heading 
+                level={2} 
+                color="lightGrey">
+                Add new dish
+            </Text.Heading>
+            <Text.Heading 
+                level={3} 
+                color="lightGrey">
+                Dish Name
+            </Text.Heading>
+            <Text.Heading 
+                level={4} 
+                color="lightGrey">
+                Ingredients
+            </Text.Heading>
+            I'm gonna be a form!
+            <Input />
+            <Text.Heading 
+                level={4} 
+                color="lightGrey">
+                Instructions
+            </Text.Heading>
+            I'm gonna be a form!
+            <Input />
+            <Button text="Save my dish" />
+        </Wrapper>
     )
 }
 
--- src/pages/Dishes/index.js
+++ src/pages/Dishes/index.js
@@ -4,21 +4,22 @@ import styles from './dishes.module.scss';
 import logoImage from '../../images/GitFoodLogo.png';
 import Image from '../../components/Image';
 import Button from '../../components/Button/Button';
-import Heading from '../../components/Text/Heading';
-import Body from '../../components/Text/Body';
+import Text from '../../components/Text/Text';
 
 
 const Dishes = () => {
 	return (
 		<div>
 			<div className={styles['dashboard-heading']}>
-				<Heading level={5} color="lightGrey">
+				<Text.Heading 
+					level={5} 
+					color="lightGrey">
 					Firstname's Dishes
-				</Heading>
+				</Text.Heading>
 				<Button text="Add a dish"/>
 			</div>
 			<div className={styles['empty-dishes']}>
-				<Body size="large">Looks Like it's empty!</Body>
+				<Text.Body size="large">Looks Like it's empty!</Text.Body>
 				<Image src={logoImage} alt="Git Food Logo" />
 				<Link to="/dishForm" className={styles.button}>Add your first dish</Link>
 				<Link to="/dish" className={styles.button}>View this dish</Link>
