dda1e243763fa607e2f358adb996e42304dfed65
Alissa Schlecht <alissaschlecht@Cathleens-MacBook-Pro.local>
2019-10-08 16:28:54 -0400

    CRUD on adding dish working, static

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,12 @@
 import React from "react";
 import styles from './button.module.scss';
 
-const Button = ({text, className}) => {
+const Button = ({text, className, onClick}) => {
 	return (
-		<button className={ `${styles.button } ${className}`}>{text}</button>
+		<button 
+			className={ `${styles.button } ${className}`}
+			onClick={onClick}
+		>{text}</button>
     )
 }
 
--- src/pages/DishForm/index.js
+++ src/pages/DishForm/index.js
@@ -6,12 +6,15 @@ import Text from '../../components/Text/Text';
 import Wrapper from '../../components/Wrapper';
 import Button from '../../components/Button/Button';
 
+const axios = require('axios');
+
 class DishForm extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      name: ''
+      name: '',
+      dishes: []
     } 
     this.updateField = this.updateField.bind(this);
   }
@@ -20,11 +23,59 @@ class DishForm extends Component {
     this.setState({ name: value });
   }
 
+  componentDidMount() {
+    fetch('https://git-food-api.herokuapp.com/api/dishes')
+      .then(response => response.json())
+      .then(json => this.setState({dishes: json.dishes}));
+  }
+
+  addDish = async () => {
+    axios.post('https://git-food-api.herokuapp.com/api/dishes', {
+      name: this.state.name
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  updateDish = async () => {
+    axios.put('https://git-food-api.herokuapp.com/api/dishes/5', {
+      name: this.state.name
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  deleteDish = async () => {
+    axios.delete('https://git-food-api.herokuapp.com/api/dishes/4')
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
 
   render(){
+    console.log(this.state.dishes);
+    console.log(this.state);
     return (
       <Wrapper>
         <Link to="/" className={styles.button}>View all dishes</Link>
+        <p>All Dishes:</p>
+        <ul>
+          {this.state.dishes.map((dish, key) => (
+            <li key={dish.id}>{dish.name}</li>
+          ))}
+        </ul>
         <Text.Heading 
           level={2} 
           color="lightGrey">
@@ -32,19 +83,36 @@ class DishForm extends Component {
         </Text.Heading>
         <Input
           id="dishName" 
-          label="Dish name"
+          label="Add Dish name"
           value={this.state.name}
           onInputChange={(value, error) => {
             this.updateField('Dishname', value, error);
           }}
         />
-
-        <br/>
-        <br/>
-        <Button text="Save my dish" />
-        <br/>
-        <br/>
-        <Button text="Delete this dish" />
+        <Button 
+          text="Save my dishname"
+          onClick={ this.addDish} 
+        />
+ <br/>
+ <br/>
+        <Input
+          id="dishName" 
+          label="Update id=5 Dish name"
+          value={this.state.name}
+          onInputChange={(value, error) => {
+            this.updateField('Dishname', value, error);
+          }}
+        />
+          <Button 
+          text="Update my dishname"
+          onClick={ this.updateDish} 
+        />
+  <br/>
+  <br/>
+      <Button 
+        text="Delete id=4 dish" 
+        onClick={ this.deleteDish} 
+      />
       </Wrapper>
     )
   }
